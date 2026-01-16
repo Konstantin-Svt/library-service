85f312fb1ddca56f96973946a08999e9ce73437d
Konstantin S. <kostaho16@gmail.com>
2026-01-16 20:09:28 +0200

    Create borrowings list & detail endpoints views

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets, mixins
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(
+    mixins.RetrieveModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet
+):
+    queryset = Borrowing.objects.select_related("book")
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+        elif self.action == "retrieve":
+            return BorrowingDetailSerializer
