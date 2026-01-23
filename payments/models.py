3bbc8c14164d15c3cd9ec01f82092c6798159027
Konstantin S. <kostaho16@gmail.com>
2026-01-23 02:01:15 +0200

    Add Payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class PaymentStatus(models.TextChoices):
+        PENDING = "PENDING", "Pending"
+        PAID = "PAID", "Paid"
+
+    class PaymentType(models.TextChoices):
+        PAYMENT = "PAYMENT", "Payment"
+        FINE = "FINE", "Fine"
+
+    status = models.CharField(choices=PaymentStatus.choices, max_length=64)
+    type = models.CharField(choices=PaymentType.choices, max_length=64)
+    borrowing = models.ForeignKey(
+        "borrowings.Borrowing",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="payments",
+    )
+    session_url = models.URLField()
+    session_id = models.IntegerField()
+    money_to_pay = models.DecimalField(decimal_places=2, max_digits=20)
+
+    def __str__(self):
+        return f"session_url: {self.session_url}, session_id: {self.session_id}, status: {self.status}"
